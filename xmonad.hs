50d3401957bd1e5b0948688538296eab04e000fe
Maxime Coste <maxime@eugensystems.com>
2010-08-31 15:49:30 +0200

    xmonad.hs: add tabbed layout

--- xmonad.hs
+++ xmonad.hs
@@ -3,6 +3,7 @@ import XMonad.Util.Run
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.DynamicLog
 import XMonad.Layout.NoBorders
+import XMonad.Layout.Tabbed
 import System.IO
 import qualified Data.Map as Map
 
@@ -16,7 +17,7 @@ myIconDir       = "/home/mawww/misc/icons/"
 dmenuCmd prompt = "dmenu -fa \"" ++ myFont ++ "\" -p \"" ++ prompt ++
                   "\" -nf \"" ++ myNormalFGColor ++ "\" -nb \"" ++ myNormalBGColor ++
                   "\" -sf \"" ++ myFocusFGColor  ++ "\" -sb \"" ++ myFocusBGColor ++ "\""
-                 
+
 
 main = do
     dzenPipe <- spawnPipe myStatusBar
@@ -28,7 +29,7 @@ main = do
         , modMask            = mod4Mask
         , workspaces         = ["web", "dev", "tmp", "bg"]
         , manageHook         = manageDocks <+> manageHook defaultConfig
-        , layoutHook         = smartBorders (avoidStruts $ layoutHook defaultConfig)
+        , layoutHook         = smartBorders $ avoidStruts $ layoutHook defaultConfig ||| tabbed shrinkText myTabConfig
         , logHook            = dynamicLogWithPP $ myDzenPP dzenPipe
         , keys               = newKeys
         }
@@ -52,12 +53,27 @@ myDzenPP h = defaultPP
          "Tall" -> icon "tall.xbm"
          "Mirror Tall" -> icon "mtall.xbm"
          "Full" -> icon "full.xbm"
+         "Tabbed Simplest" -> icon "tabbed.xbm"
          _ -> x
         )
     , ppOutput = hPutStrLn h
     }
     where
     dropIx wsId = if (':' `elem` wsId) then drop 2 wsId else wsId
+
+myTabConfig = defaultTheme
+    { activeColor         = myFocusBGColor
+    , activeBorderColor   = myFocusBGColor
+    , activeTextColor     = myFocusFGColor
+    , inactiveColor       = myNormalBGColor
+    , inactiveBorderColor = myNormalBGColor
+    , inactiveTextColor   = myNormalFGColor
+    , urgentColor         = myNormalBGColor
+    , urgentBorderColor   = myNormalBGColor
+    , urgentTextColor     = myNormalFGColor
+    , fontName            = myFont
+    , decoHeight          = 16
+    }
 
 newKeys x = Map.union (Map.fromList (myKeys x)) (keys defaultConfig x)
 
